5d57d2b70f71c4d4b687ac0debee0aef3fbd4304
Renan H. Muratori <renan.muratori@gmail.com>
2024-07-10 21:15:38 -0300

    Adicionando dockerfile e projeto maven 2

---
+++ inf335-atividade5_2/src/test/java/br/unicamp/ic/inf335/app/OlaUnicampTest.java
@@ -0,0 +1,20 @@
+package br.unicamp.ic.inf335.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class OlaUnicampTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
